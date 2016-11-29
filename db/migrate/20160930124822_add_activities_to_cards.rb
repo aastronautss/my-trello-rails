9be3fc39b1f8c79dc24af4cfe0f693d44a72daa4
Tyler Guillen <tyguillen@gmail.com>
2016-11-28 20:15:21 -0500

    fix comment migration

--- db/migrate/20160930124822_add_activities_to_cards.rb
+++ db/migrate/20160930124822_add_activities_to_cards.rb
@@ -2,9 +2,5 @@ class AddActivitiesToCards < ActiveRecord::Migration
   def change
     add_column :cards, :activities, :jsonb
     add_index :cards, :activities, using: :gin
-
-    Comment.all.each do |comment|
-      comment.card.add_comment comment.body, comment.author
-    end
   end
 end
