7bc9ce48c93cc889066608b0ce392bf058755353
Tyler Guillen <tyguillen@gmail.com>
2016-12-01 15:45:47 -0500

    add host to production mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,11 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = {
+    host: 'my-trello-rails.heroku.com',
+    protocol: 'https'
+  }
+
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
