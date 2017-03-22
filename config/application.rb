c43f345eb0e75a70ff5e59b990a31688019db01a
Tyler Guillen <tyguillen@gmail.com>
2017-03-22 08:57:04 -0400

    change autoloader

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module MyTrelloRails
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.eager_load_paths << "lib"
+    config.autoload_paths += Dir["#{config.root}/app/models/**/", "#{config.root}/lib/**/"]
   end
 end
